3c8d4cf0103a607c2a86b98927cce43bd8ed4d1a
Yuri Borisov <yuri.borisov.v@gmail.com>
2018-09-18 22:22:33 +0300

    Profiler: duration ind. Not tested

--- src/profiler.h
+++ src/profiler.h
@@ -18,28 +18,19 @@ enum {
 class Pind: public MPind
 {
     public:
-	/** Type of events register */
-	using TEventsMap = std::map<PEvent::TId, const PEvent*>;
-	using TEventsMapElem = std::pair<PEvent::TId, const PEvent*>;
-	using TEvents = std::vector<PEvent>;
 	/** Initialization data */
 	struct Idata {
-	    Idata(const string& aDescription, int aBufLim, const TEvents* aEvents):
-		mDescription(aDescription), mBufLim(aBufLim), mEvents(aEvents) {}
+	    Idata(const string& aDescription): mDescription(aDescription) {}
 	    string mDescription; /** Description */
-	    int mBufLim; /** Buffer size limit */
-	    const TEvents* mEvents; /** Events map */
 	};
     public:
-	Pind(const string& aDescription, int aBufLenLIm, const TEvents& aEvents);
-	Pind(const Idata& aIdata): Pind(aIdata.mDescription, aIdata.mBufLim, *(aIdata.mEvents)) {}
+	Pind(const string& aDescription, int aBufLenLIm): mDescription(aDescription), mBufLenLim(aBufLenLIm) {}
 	virtual ~Pind() {}
     public:
 	// From MPind
 	virtual string getDescription() override { return mDescription;}
 	virtual int getBufLen() const override { return -1;}
 	virtual int getBufLenLimit() const override { return mBufLenLim;}
-	virtual const PEvent& getEvent(PEvent::TId aId) const override;
 	/** Obtains clock resolution in nanoseconds */
 	virtual TClock getClockResolution() const override;
 	virtual bool saveBufToFile(const string& aFilePath) const { return false;}
@@ -50,14 +41,12 @@ class Pind: public MPind
 	/** Gets clock absolute value */
 	TClock GetClock() const;
     protected:
-	/** Limit of buf len - predefined size of buffer */
-	int mBufLenLim;
 	/** Description */
 	string mDescription;
+	/** Limit of buf len - predefined size of buffer */
+	int mBufLenLim;
 	/** Insertion position in events buffer */
 	int mPos = -1;
-	/** Events registered */
-	TEventsMap mEvents;
 };
 
 /** Record for performance indicator 'Clock' */
@@ -79,31 +68,79 @@ class PindClock: public Pind
 {
     public:
 	static const int KId = EPiid_Clock;
+	/** Type of events register */
+	using TEventsMap = std::map<PEvent::TId, const PEvent*>;
+	using TEventsMapElem = std::pair<PEvent::TId, const PEvent*>;
+	using TEvents = std::vector<PEvent>;
     public:
+	/** Initialization data */
 	struct Idata: public Pind::Idata {
-	    Idata(const string& aDescription, int aBufLim, const TEvents* aEvents): Pind::Idata(aDescription, aBufLim, aEvents) {}
+	    Idata(const string& aDescription, int aBufLim, const TEvents* aEvents): Pind::Idata(aDescription), mBufLim(aBufLim), mEvents(aEvents) {}
+	    string mDescription; /** Description */
+	    int mBufLim; /** Buffer size limit */
+	    const TEvents* mEvents; /** Events map */
 	};
     public:
 	PindClock(const Idata& aIdata);
 	virtual ~PindClock();
 	void Rec(PEvent::TId aEventId, MElem* aNode);
+	// From MPind
+	virtual const PEvent& getEvent(PEvent::TId aId) const override;
 	virtual string recToString(int aRecNum) const override;
 	virtual string getFileSuf() const override { return "clock";}
     public:
 	virtual PRec* NewRec();
     protected:
+	/** Events registered */
+	TEventsMap mEvents;
 	/** Events buffer */
 	PRecClock* mBuf;
 };
 
+/** Event for duration indicator */
+class PEventDur: public PEvent
+{
+    public:
+	/** Constructor
+	 * @param aStart - sign of measured interval start
+	 */
+	PEventDur(TId aId = KUndefEid, const string& aDescription = string()): PEvent(aId, aDescription) {} 
+    public:
+	bool mStart; /** Sign of measured interval start */
+};
+
 /** Performance indicator: event to event duration */
-class PindDur: public PindClock
+class PindDur: public Pind
 {
     public:
 	static const int KId = EPiid_Dur;
+	using TEventsMap = std::map<PEvent::TId, const PEventDur*>;
+	using TEventsMapElem = std::pair<PEvent::TId, const PEventDur*>;
+	using TEvents = std::vector<PEventDur>;
+	struct TBufData { Pind::TClock mStartClock = 0, mDur = 0; TBufData(Pind::TClock aStart, Pind::TClock aDur): mStartClock(aStart), mDur(aDur) {}};
+	using TBuf = map<PEvent::TId, TBufData>;
+	using TBufElem = pair<PEvent::TId, TBufData>;
+    public:
+	/** Initialization data */
+	struct Idata: public Pind::Idata {
+	    Idata(const string& aDescription, const TEvents& aEvents):
+		Pind::Idata(aDescription), mEvents(aEvents) {}
+	    string mDescription; /** Description */
+	    TEvents mEvents; /** Events */
+	};
     protected:
+	PindDur(const Idata& aIdata);
+	virtual ~PindDur();
+	void Rec(PEvent::TId aEventId, bool aStart = false);
 	/** Finds nearest lower record for base event */
 	PRec* FindBaseRec(int aPos) const;
+	// From MPind
+	virtual const PEvent& getEvent(PEvent::TId aId) const override;
+    protected:
+	/** Events registered */
+	TEventsMap mEvents;
+	TBuf mBuf;
+
 };
 
 
